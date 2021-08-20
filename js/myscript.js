2cb7683a751a5fd7709becb28143f072f7e8bf8b
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-20 13:39:56 -0400

    add js

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+// simple model values
+let n = 1
+let complicated = {id:3, address:"here", list:[4,3,2], signed_in:false}
+
+const fn = (x, y, ) => {return x+y}
+
+n = fn(4,2)
+console.log(n, complicated)
+// here we inject new content into the web page
+// we use back-ticks for string interpolation
+let some_text = `Address: ${complicated.address} ${complicated.array[2]}`
+content.innerHTML = some_text
+
+// add an event listener to the button
+const handleButton = ()=>{console.log('clicked!')}
+btnDoStuff.addEventListener('click', handleButton)
