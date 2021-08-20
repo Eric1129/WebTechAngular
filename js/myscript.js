82fa94fcaf349388e8b457dd64c2e331415cd042
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-20 15:09:57 -0400

    add api functions

--- js/myscript.js
+++ js/myscript.js
@@ -17,10 +17,38 @@ btnDoStuff.addEventListener('click', handleButton)
 
 //fetching data from an API end point
 const getData = ()=>{
+    let end_point_url = "https://swapi.dev/api"
+    //
+    let category = swcategory.value // "planets"
+    let id = swid.value //3
     // NB fetch is asynchronous
-    fetch("http://example.com/movies.json").then(Response => Response.json())
-    .then(data => console.log(data))
+    fetch(`${end_point_url}/${category}/${id}`)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data)
+        // lets put all the data into the page temporarily
+        //
+        let swapi_text = JSON.stringify(data)
+        swapi.innerHTML = swapi_text
+    })
 }
 
 btnGetAPI.addEventListener("click", getData)
 
+function oldWay(params){
+    return params
+}
+
+// a few words about functions
+// now...
+const f = (x)=>{return x*x};
+// we can leave out all the brackets if it's dead simple
+const g = x=>x*x
+// if there are NO params or more than one, we MUST use the brackets
+const h = ()=>"oh"
+// if there is more than one line in the function body
+// we MUST have curly braces
+const j = v=>{
+    result = `${v} stuff`
+    return result // explicitly returns something
+}
